b259f7b05c70125e8fa4925c4fb374b25ef66fdf
camcl <camille.clouard@scilifelab.uu.se>
2025-09-17 11:50:54 +0200

    fix: removed prints to file

--- workflow/scripts/mosdepth_merge_timestep.py
+++ workflow/scripts/mosdepth_merge_timestep.py
@@ -7,35 +7,34 @@ For each timestep, this script aggregates the average coverage for each target f
 
 os.makedirs(snakemake.output.outdir, exist_ok=True)
 
-with open(snakemake.log, "w") as log:
-    for nbamdir in os.listdir(snakemake.input.indir):
-        print(os.path.join(snakemake.input.indir, nbamdir), file=log)
-        if os.path.isdir(os.path.join(snakemake.input.indir, nbamdir)):  # skip files that might be in the input directory
-            print(nbamdir, file=snakemake.log)
-            summaries = []
-            for target in snakemake.config.get("amplicons") + snakemake.config.get("extra_regions"):
-                summary = os.path.join(snakemake.input.indir, nbamdir, f"{target}.mosdepth.summary.txt")
-                if os.path.exists(summary):
-                    summaries.append(summary)
-                else:
-                    print(f"Warning: {summary} does not exist. Skipping.", file=log)
-    
-            cols = ["length", "bases", "mean", "target"]
-            df = pd.concat([pd.read_csv(summary, sep='\t')
-                           .assign(target=os.path.basename(summary).replace(".mosdepth.summary.txt", ""))
-                            for summary in summaries
-                            ])
-            print(df, file=log)
-            try:
-                df = df[df["chrom"] == "total_region"].loc[:, cols]
-            except KeyError:  # this might not be necessary
-                df = pd.DataFrame([0.0, 0.0, 0.0, 0.0], columns=cols)
-            print(df, file=log)
-            try:
-                df.set_index("target", inplace=True)
-            except AttributeError:  # if pd.Series is returned
-                df.rename("target", inplace=True)
-            df.to_csv(
-                os.path.join(snakemake.output.outdir, f"timestep{os.path.basename(nbamdir)}_coverage_per_amplicon.csv"),
-                index=True
-            )
+for nbamdir in os.listdir(snakemake.input.indir):
+    print(os.path.join(snakemake.input.indir, nbamdir))
+    if os.path.isdir(os.path.join(snakemake.input.indir, nbamdir)):  # skip files that might be in the input directory
+        print(nbamdir)
+        summaries = []
+        for target in snakemake.config.get("amplicons") + snakemake.config.get("extra_regions"):
+            summary = os.path.join(snakemake.input.indir, nbamdir, f"{target}.mosdepth.summary.txt")
+            if os.path.exists(summary):
+                summaries.append(summary)
+            else:
+                print(f"Warning: {summary} does not exist. Skipping.")
+
+        cols = ["length", "bases", "mean", "target"]
+        df = pd.concat([pd.read_csv(summary, sep='\t')
+                       .assign(target=os.path.basename(summary).replace(".mosdepth.summary.txt", ""))
+                        for summary in summaries
+                        ])
+        print(df)
+        try:
+            df = df[df["chrom"] == "total_region"].loc[:, cols]
+        except KeyError:  # this might not be necessary
+            df = pd.DataFrame([0.0, 0.0, 0.0, 0.0], columns=cols)
+        print(df)
+        try:
+            df.set_index("target", inplace=True)
+        except AttributeError:  # if pd.Series is returned
+            df.rename("target", inplace=True)
+        df.to_csv(
+            os.path.join(snakemake.output.outdir, f"timestep{os.path.basename(nbamdir)}_coverage_per_amplicon.csv"),
+            index=True
+        )
