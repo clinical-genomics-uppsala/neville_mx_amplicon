4274f83ab2b68942948b904e364d3d48cc338bae
camcl <camille.clouard@scilifelab.uu.se>
2026-07-01 15:19:17 +0200

    feat: output tables of artifacts and background noise

---
+++ workflow/scripts/create_artifact_file_longread.py
@@ -0,0 +1,111 @@
+import gzip
+import statistics
+from collections import OrderedDict
+
+
+vcf_files = snakemake.input.vcfs
+artifact_panel_path = snakemake.output.artifact_panel
+callers_used = snakemake.params.callers
+print(f"Callers used by the pipeline: {callers_used}\n----------------------------------------------------\n")
+
+# Dictionary with count of observations of a position and AF for each observation
+call_stats_dict = {}
+
+
+def get_info_field(info_str, key):
+    """Safely extracts a value from the INFO field string."""
+    for field in info_str.split(";"):
+        if field.startswith(f"{key}="):
+            return field.split("=", 1)[1]
+    return None
+
+
+def get_format_field(format_str, data_str, key):
+    """Safely extracts a value from the FORMAT field string."""
+    fields = data_str.split(":")
+    keys = format_str.split(":")
+    format_dict = dict(zip(keys, fields))
+    return format_dict[key]
+
+
+for file_name in vcf_files:
+    print(f"call_stats_dict -- Processing {file_name}")
+    with gzip.open(file_name, "rt") as infile:
+        for line in infile:
+            if line.startswith("#"):
+                continue
+
+            columns = line.strip().split("\t")
+            if len(columns) < 8:
+                continue
+
+            chrom = columns[0]
+            pos = int(columns[1])
+            ref = columns[3]
+            alt = columns[4]
+            variant_type = "SNV" if (len(ref) == 1 and len(alt) == 1) else "INDEL"
+            info = columns[7]
+            formats = columns[8]
+            data = columns[9]
+
+            key = (chrom, pos, variant_type)
+
+
+            callers_str = get_info_field(info, "CALLERS")
+            print(f"\nVariant processed: {(chrom, pos, variant_type)} -- Callers {callers_str}")
+            if not callers_str:
+                continue
+            callers = callers_str.split(",")
+            print(f"\nVariant processed: {(chrom, pos, variant_type)} -- Callers SPLIT {callers}")
+
+            af_str = get_format_field(formats, data, "AF")
+            if not af_str:
+                continue
+            else:
+                af = float(af_str)
+
+            if key not in call_stats_dict:
+                call_stats_dict[key] = {}  # {c: [0, []] for c in callers}
+
+            for caller in callers:
+                if caller not in call_stats_dict[key]:
+                    call_stats_dict[key][caller] = [1, []]
+                else:
+                    call_stats_dict[key][caller][0] += 1
+                    call_stats_dict[key][caller][1].append(af)
+
+# Sort the variants per genomic position
+call_stats_dict_sorted = OrderedDict(sorted(call_stats_dict.items()))
+print('\n')
+print(call_stats_dict_sorted)
+print('\n')
+
+with open(artifact_panel_path, "w") as artifact_panel:
+    header = ["Chromosome", "pos", "variant_type"]
+    for caller in callers_used:
+        header.extend([caller, "median_MAF", "sd_MAF"])
+    artifact_panel.write("\t".join(header) + "\n")
+
+    for key, callers_data in call_stats_dict_sorted.items():
+        chrom, pos, variant_type = key
+        row = [str(chrom), str(pos), str(variant_type)]
+        for caller in callers_used:
+            obs_count = 0
+            median_af = 0
+            sd_af = 1000
+            if caller not in callers_data:
+                print(f"{caller} not in {callers_data}, extending row...\n")
+                row.extend([f"{obs_count}", f"{median_af}", f"{sd_af}"])
+            else:
+                obs_count, af_list = callers_data[caller]
+                af_list.sort()
+                if len(af_list) >= 4:
+                    print(f"Enough occurences of the position ({len(af_list)} observations), computing statistics for it...")
+                    median_af = statistics.median(af_list)
+                    sd_af = statistics.stdev(af_list)
+                row.extend([f"{obs_count}", f"{median_af}", f"{sd_af}"])
+        if sum([int(n) for n in row[3::3]]) == 0:
+            print(f"\nNull counts for {row}")
+            print(f"Data: {call_stats_dict[key]}\n")
+        artifact_panel.write("\t".join(row) + "\n")
+
---
+++ workflow/scripts/create_background_file_longread.py
@@ -0,0 +1,92 @@
+import gzip
+import statistics
+from collections import OrderedDict
+
+
+gvcf_files = snakemake.input.gvcfs
+background_file_path = snakemake.output.background_file
+min_dp = snakemake.params.min_dp
+max_af = snakemake.params.max_af
+
+background_dict = {}
+
+for file_name in gvcf_files:
+    with gzip.open(file_name, "rt") as infile:
+        for line in infile:
+            if line.startswith("#"):
+                continue
+
+            columns = line.strip().split("\t")
+            if len(columns) < 10:
+                continue
+
+            # Handle potential chromosome prefixes
+            chrom = columns[0]
+            if chrom.startswith("chr"):
+                chrom = chrom[3:]
+
+            pos = columns[1]
+            key = (chrom, pos)
+
+            formats = columns[8].split(":")
+            data = columns[9].split(":")
+
+            try:
+                ad_idx = formats.index("AD")
+                ad_info = data[ad_idx].split(",")
+                # handle specific format in ClairS-TO for RefCalls, for example GT:GQ:DP:AF:AD:AU:CU:GU:TU   0/0:8:7181:0.9109:6541:2:6541:1:0 --> count for alt alleles are :-separated
+                if len(ad_info) == 1:
+                	ad_info = data[ad_idx:]  # AD:AU:CU:GU:TU    6541:2:6541:1:0
+                	print(ad_info)
+
+                ref_ad = int(ad_info[0])  # 6541
+                alt_ad = sum(int(ad) for ad in ad_info[1:])  # sum([2, 6541, 1, 0]) = 6544
+                # or alt_ad = sum(int(ad) for ad in ad_info[1:]) - int(max(ad_info[1:]))  to sum only the alternate alleles
+                dp = ref_ad + alt_ad
+
+                if dp <= min_dp:
+                    continue
+
+                alt_af = alt_ad / float(dp)  # 1.0005
+                
+                # Correct VAF for RefCalls in ClairS-TO
+                if alt_af > 1:
+                    alt_af = alt_af - 1  # 0.0005 < max_af=0.03
+
+                # Check if it's potentially a homozygous variant being filtered
+                if alt_af > 1 - max_af:  # 1.0005 > 1 - 0.03
+                    alt_af = 1 - alt_af
+
+                if alt_af > max_af:
+                    continue
+
+                if key in background_dict:
+                    background_dict[key].append(alt_af)
+                else:
+                    background_dict[key] = [alt_af]
+            except (ValueError, IndexError):
+                # Skip if AD field or index is missing/malformed
+                continue
+            
+# Sort the variants per genomic position
+background_dict_sorted = OrderedDict(sorted(background_dict.items()))
+
+# Write background_dict to json file
+# json_file_path = snakemake.output.background_json
+# import json
+# with open(json_file_path, 'w') as json_file:
+#     json.dump(background_dict_sorted, json_file)
+
+with open(background_file_path, "w") as background_file:
+    background_file.write("Chr\tPos\tMedian_or_Mean\tSD\tNrObs\n")
+    for key, af_list in background_dict_sorted.items():
+        if len(af_list) < 4 and len(af_list) > 0:
+            median_background = statistics.mean(af_list)
+            stdev_background = 0
+        else:
+            median_background = statistics.median(af_list)
+            stdev_background = statistics.stdev(af_list)
+
+        chrom, pos = key
+        background_file.write(f"{chrom}\t{pos}\t{median_background}\t{stdev_background}\t{len(af_list)}\n")
+
