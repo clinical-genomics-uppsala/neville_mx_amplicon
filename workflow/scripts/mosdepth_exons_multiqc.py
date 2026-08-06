6ba5b7ce0cc98ccc5ffe3ca5ff6f3283e961209a
camcl <camille.clouard@scilifelab.uu.se>
2026-08-06 13:21:32 +0200

    fix: make exon as first column to get rows parsed as unique in multiqc

--- workflow/scripts/mosdepth_exons_multiqc.py
+++ workflow/scripts/mosdepth_exons_multiqc.py
@@ -10,10 +10,10 @@ with gzip.open(snakemake.input.bed, "rt") as fin, open(snakemake.output.tsv, "w"
     fout.write("#   id: exon_coverage_table\n")
     fout.write("#   title: Coverage per exon\n")
     fout.write("#   namespace: Mosdepth\n")
-    fout.write("Chromosome\tStart\tEnd\tExon\tMean coverage\n")
+    fout.write("Exon\tChromosome\tStart\tEnd\tMean coverage\n")
 
     for line in fin:
         chrom, start, end, exon, coverage = line.rstrip().split("\t")[:5]
         fout.write(
-            f"{chrom}\t{start}\t{end}\t{exon}\t{coverage}\n"
+            f"{exon}\t{chrom}\t{start}\t{end}\t{coverage}\n"
         )
